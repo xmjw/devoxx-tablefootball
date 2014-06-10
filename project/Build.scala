fbdcb73123f5f1255c289deadba1917358844b48
Michael Wawra <wawra@twilio.com>
2014-06-10 15:30:09 +0100

    adding missing dependency for heroku;

--- project/Build.scala
+++ project/Build.scala
@@ -10,8 +10,8 @@ object ApplicationBuild extends Build {
     val appDependencies = Seq(
       // Add your project dependencies here,
       "postgresql" % "postgresql" % "9.1-901.jdbc4",
-      "com.twilio.sdk" % "twilio-java-sdk" % "3.4.5"
-      
+      "com.twilio.sdk" % "twilio-java-sdk" % "3.4.5",
+      "org.ow2.spec.ee" % "ow2-validation-1.1-spec" % "1.0.0-M1"
     )
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
