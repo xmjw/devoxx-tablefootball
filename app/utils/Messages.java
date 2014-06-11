745d3e3168ceeb3100e5b6371959a0398ee7ba4e
Michael Wawra <wawra@twilio.com>
2014-06-11 12:07:30 +0100

    adding a few tweeks

--- app/utils/Messages.java
+++ app/utils/Messages.java
@@ -6,11 +6,11 @@ import models.*;
 public class Messages {  
   //In use...
   public static String NonMemberHelp() { return "Hello! If you'd like to play Twilio-Devoxx Table Football, SMS this number with 'join' and you'll get further instructions."; }
-  public static String MemberHelp() { return "Text 'play <teamname>' to play, or just 'play' if you'd like to play a random team, and we'll allocate one for you."; }
+  public static String MemberHelp() { return "Text 'play' if you'd like to play a match, and we'll allocate one for you. If you've finished a game, send 'score n' where n is your score. If you don't want to play a game, send 'abort' and we'll cancel a fixture."; }
   public static String Win(Team against) { return "Congratulations! You won your match against "+against.name+". It certainly isn't over! Text back 'play' to find your next opponent."; }
   public static String Loss(Team against) { return "Bad luck! You lost your match against "+against.name+". Don't think it's all over? Text back 'play' and find your next opponent."; }
   public static String Draw(Team against) { return "Wow! You drew against team "+against.name+"! Oh well. Best play one more ball and see who wins that! Text back 'score 1' if you win, 'score 1' if you lose... Good luck..."; }
-  public static String Play(Team against) { return "It's almost kick off! You've been fixed up to play "+against.name+"! Head to the Football Tables to get started. Text back 'help' if you're having problems."; }
+  public static String Play(Team against) { return "It's almost kick off! You've been fixed up to play "+against.name+"! Head to the Football Tables to get started. Text back 'help' if you're having problems. Text 'score n' to record your score - where 'n' is the number of goals you scored."; }
   public static String NoPlay() { return "We're sorry, we couldn't find a team to match you up against. Why not play a friendly with your team mate? Text back 'play' in a few minutes to try and find another opponent."; }
   public static String ScorePending() { return "Thanks! We're just waiting for your opponent to confirm their score so we can declare a winner."; }
   public static String Abort() { return "You current match has been cancelled. Text back 'play' to find an opponent."; }
